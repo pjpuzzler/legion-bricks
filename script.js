38dde4637b4ccea4cfd7ce8adcba0f2fc9488ffa
prj7465 <prj7465@rit.edu>
2023-05-17 10:21:16 -0400

    c

--- script.js
+++ script.js
@@ -1,38 +1,81 @@
 window.onload = () => {
-    drawBricksCanvas();
+    updateCanvas();
+
+    const elPicker = document.getElementById("picker");
+
+    elPicker.innerHTML = "<option selected></option>";
+
+    for (const brick of BRICKS)
+        elPicker.add(new Option(brick.getString(), brick.coords));
 };
 
+window.onresize = updateCircle;
+
+let highlightX = null,
+    highlightY = null,
+    highlightVertical = null;
+
+function updateCanvas() {
+    drawBricksCanvas();
+    updateCircle();
+}
+
+function updateCircle() {
+    const elCircle = document.getElementById("circle");
+
+    if (document.getElementById("picker").value !== "") {
+        const elBricksCanvas = document.getElementById("bricks-canvas");
+        if (highlightVertical) {
+            elCircle.style.left = `${
+                elBricksCanvas.offsetLeft + highlightX + 0.5 + brickHeight / 2
+            }px`;
+            elCircle.style.top = `${
+                elBricksCanvas.offsetTop + highlightY + 0.5
+            }px`;
+        } else {
+            elCircle.style.left = `${
+                elBricksCanvas.offsetLeft + highlightX + 0.5 + brickWidth / 2
+            }px`;
+            elCircle.style.top = `${
+                elBricksCanvas.offsetTop + highlightY + 0.5 - brickHeight / 2
+            }px`;
+        }
+        elCircle.style.display = "initial";
+    } else elCircle.style.display = "none";
+}
+
 function drawBricksCanvas() {
+    const highlightedCoords = document.getElementById("picker").value;
+
     const canvas = document.getElementById("bricks-canvas");
     const ctx = canvas.getContext("2d");
-
-    const brickHeight = 4,
-        brickWidth = 8;
 
     canvas.height = (brickHeight * n) / 2;
     canvas.width = brickHeight * m * 2;
 
-    const brickColor = "#FFA500",
-        wallColor = "#000000",
-        brickHighlightColor = "#9EFF00",
-        brickOutlineColor = "#000000";
-
     ctx.translate(0.5, -brickHeight + 0.5);
 
     function drawBrick(i, j, x, y, color, vertical) {
+        const coords = `${i}-${j}`;
         ctx.beginPath();
         if (vertical) ctx.rect(x, y, brickHeight, brickWidth);
         else ctx.rect(x, y, brickWidth, brickHeight);
         ctx.fillStyle =
-            `${i}-${j}` === TEST
+            coords === highlightedCoords
                 ? brickHighlightColor
-                : `${i}-${j}` in BRICKS
-                ? "red"
+                : COORDS.has(coords)
+                ? brickColor2
                 : color;
         ctx.strokeStyle = brickOutlineColor;
         ctx.lineWidth = 1;
         ctx.fill();
         ctx.stroke();
+
+        if (coords === highlightedCoords) {
+            highlightX = x;
+            highlightY = y;
+            highlightVertical = vertical;
+        }
     }
 
     for (let i = 0; i < n; i++) {
@@ -43,6 +86,37 @@ function drawBricksCanvas() {
             let color;
 
             if (
+                (Math.abs(
+                    Math.pow(x - canvas.width / 2, 2) +
+                        Math.pow(y - canvas.height / 2 - brickHeight * 25, 2) -
+                        Math.pow(260, 2)
+                ) < 2100 &&
+                    y <= canvas.height - brickHeight * 21) ||
+                ((x < brickHeight * 39 ||
+                    x > canvas.width - brickHeight * 41) &&
+                    y > canvas.height - brickHeight * 17 &&
+                    y < canvas.height - brickHeight * 14) ||
+                (((x < brickHeight * 39 && x > brickHeight * 6) ||
+                    (x > canvas.width - brickHeight * 41 &&
+                        x < canvas.width - brickHeight * 6)) &&
+                    y > canvas.height - brickHeight * 21 &&
+                    y < canvas.height - brickHeight * 18) ||
+                (((x < brickHeight * 41 && x > brickHeight * 39) ||
+                    (x > canvas.width - brickHeight * 41 &&
+                        x < canvas.width - brickHeight * 39)) &&
+                    y > canvas.height - brickHeight * 21 &&
+                    y < canvas.height - brickHeight * 14) ||
+                (x < brickHeight * 78 &&
+                    x > brickHeight * 70 &&
+                    y < brickHeight * 44 &&
+                    y > brickHeight * 39) ||
+                (y > brickHeight * 65 &&
+                    (x < brickHeight * 2 ||
+                        x > canvas.width - brickHeight * 3)) ||
+                y > brickHeight * 78
+            )
+                color = edgeColor;
+            else if (
                 (Math.pow(x - canvas.width / 2, 2) +
                     Math.pow(y - canvas.height / 2 - brickHeight * 24, 2) >
                     Math.pow(260, 2) &&
@@ -52,7 +126,7 @@ function drawBricksCanvas() {
                     y > canvas.height - brickHeight * 21 &&
                     y < canvas.height - brickHeight * 14)
             )
-                color = wallColor;
+                continue;
             else color = brickColor;
 
             if (i % 4 == 0) {
